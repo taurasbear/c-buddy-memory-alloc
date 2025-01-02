6025920483c0a392501363217c2b0d1ff631ffc1
Tauras Narvilas <youngicorn@gmail.com>
2025-01-02 16:20:38 +0000

    initial commit

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#define MEMORY_POOL_SIZE 1024
+
+typedef struct BlockHeader
+{
+    int size;
+    bool free;
+    struct BlockHeader *left;
+    struct BlockHeader *right;
+
+} BlockHeader;
+
+static uint8_t memory_pool[MEMORY_POOL_SIZE];
+
+static BlockHeader *free_btree;
+
+void initialize_memory_allocator()
+{
+    free_btree = (BlockHeader *)memory_pool;
+    free_btree->size = MEMORY_POOL_SIZE - sizeof(BlockHeader);
+    free_btree->left = NULL;
+    free_btree->right = NULL;
+}
+
+int main()
+{
+    initialize_memory_allocator();
+
+    printf("Hello World!");
+
+    return 0;
+}
